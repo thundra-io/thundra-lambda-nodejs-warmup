4eb4d2124941b6faf11055bfcef634e5c5d1a25c
Francisco Guimarães <francisco.cpg@gmail.com>
2018-04-26 14:28:30 -0300

    Using arrow functions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import WarmupHandler from "./warmup-handler";
 module.exports = extraCallback => {
     return originalFunction => {
         const warmupHandler = new WarmupHandler(extraCallback);
-        return async function (event, context, callback) {
+        return async (event, context, callback) => {
             const warmupCallback = typeof callback === "function" ? callback : context.succeed;
             if (!warmupHandler.checkAndHandleWarmupRequest(event, warmupCallback)) {
                 await originalFunction(event, context, callback);
